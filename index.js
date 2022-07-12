4a20ca2f2270744bcea672b1e6bade25a830daf1
Lenna <pampam32121@gmail.com>
2022-07-12 21:47:47 +0300

    displayed 100 random colors

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 'use strict';
 
-const pathAPI = 'https://apis.scrimba.com/hexcolors';
+const scrimbaAPI = 'https://apis.scrimba.com/hexcolors';
+let count = 100
+
+function displayColors(colors) {
+    const colorsHTML = colors.map(color => {
+        return `<div class="my-color" style="background-color: ${color.value}"></div>`;
+    }).join('');
+
+    document.body.innerHTML = `
+        <div class="colors">
+            ${colorsHTML}
+        </div>
+    `;
+}
+
+async function getColors(num) {
+    const response = await fetch(`${scrimbaAPI}?count=${num}`);
+    const data = await response.json();
+    const colors = data.colors;
+    displayColors(colors);
+}
+
+getColors(count);
